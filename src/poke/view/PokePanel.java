e6fd285719c8fcfa61a9f04628c2dcad8568f3c8
HunterKnott <hunterknott00@gmail.com>
2019-02-04 13:04:06 -0700

    Added the PokePanel class

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -1,6 +1,47 @@
 package poke.view;
 
-public class PokePanel
-{
+import javax.swing.*;
+import poke.controller.PokeController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.event.AncestorListener;
+import javax.swing.event.AncestorEvent;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.awt.Point;
+import java.awt.Dimension;
+import java.awt.Component;
+import java.awt.Font;
 
+public class PokePanel extends JPanel
+{
+	private PokeController appController;
+	private SpringLayout appLayout;
+	
+	public PokePanel(PokeController appController)
+	{
+		super();
+		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.WHITE);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
