a4883e5bfccc569ef1c044b3dbec8b4446a3293a
HunterKnott <hunterknott00@gmail.com>
2019-02-12 12:27:22 -0700

    Added the data members to the constructor and setupPanel method

--- src/poke/view/PokedexPanel.java
+++ src/poke/view/PokedexPanel.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Dimension;
 import poke.controller.PokedexController;
 
 public class PokedexPanel extends JPanel
@@ -12,7 +13,7 @@ public class PokedexPanel extends JPanel
 	private SpringLayout appLayout;
 	
 	private JButton changeButton;
-	private JComboBox pokedexDropDown;
+	private JComboBox pokedexDropdown;
 	
 	private JTextField numberField;
 	private JTextField nameField;
@@ -32,9 +33,27 @@ public class PokedexPanel extends JPanel
 	public PokedexPanel(PokedexController appController)
 	{
 		super();
-		this.app = appController;
+		this.app = app;
 		
 		this.appLayout = new SpringLayout();
+		
+		numberField = new JTextField("0");
+		nameField = new JTextField("A Pokemon");
+		evolveField = new JTextField("true");
+		attackField = new JTextField("0");
+		enhancementField = new JTextField("0");
+		healthField = new JTextField("0");
+		
+		healthLabel = new JLabel("Health: ");
+		numberLabel = new JLabel("# ");
+		
+		evolveLabel = new JLabel("This pokemon can evolve");
+		attackLabel = new JLabel("Attack: ");
+		enhanceLabel = new JLabel("Enhancements: ");
+		nameLabel = new JLabel("Name: ");
+		imageLabel = new JLabel("Pokemon: ");
+		changeButton = new JButton("Change");
+		pokedexDropdown = new JComboBox("Click here to see other pokemon");
 		
 		setupPanel();
 		setupLayout();
@@ -43,7 +62,27 @@ public class PokedexPanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(1000, 500));
 		this.setBackground(Color.WHITE);
+		
+		this.add(pokedexDropdown);
+		this.add(numberField);
+		this.add(nameField);
+		this.add(evolveField);
+		this.add(attackField);
+		this.add(enhancementField);
+		this.add(healthField);
+		
+		this.add(healthLabel);
+		this.add(numberLabel);
+		this.add(evolveLabel);
+		this.add(attackLabel);
+		this.add(enhanceLabel);
+		this.add(nameLabel);
+		this.add(imageLabel);
+		this.add(changeButton);
+		this.add(pokedexDropdown);
 	}
 	
 	private void setupLayout()
