8d723411db99637e8b97e89d517348ce000538ae
HunterKnott <hunterknott00@gmail.com>
2019-02-12 12:03:23 -0700

    Put data members in PokedexPanel

--- src/poke/view/PokedexPanel.java
+++ src/poke/view/PokedexPanel.java
@@ -1,29 +1,40 @@
 package poke.view;
 
 import javax.swing.*;
-import poke.controller.PokedexController;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Color;
-import javax.swing.event.AncestorListener;
-import javax.swing.event.AncestorEvent;
-import java.awt.event.ContainerAdapter;
-import java.awt.event.ContainerEvent;
-import java.awt.Point;
-import java.awt.Dimension;
-import java.awt.Component;
-import java.awt.Font;
+import poke.controller.PokedexController;
 
 public class PokedexPanel extends JPanel
 {
-	private PokedexController appController;
+	private PokedexController app;
 	private SpringLayout appLayout;
+	
+	private JButton changeButton;
+	private JComboBox pokedexDropDown;
+	
+	private JTextField numberField;
+	private JTextField nameField;
+	private JTextField evolveField;
+	private JTextField attackField;
+	private JTextField enhancementField;
+	private JTextField healthField;
+	
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel evolveLabel;
+	private JLabel attackLabel;
+	private JLabel enhanceLabel;
+	private JLabel healthLabel;
+	private JLabel imageLabel;
 	
 	public PokedexPanel(PokedexController appController)
 	{
 		super();
-		this.appController = appController;
-		appLayout = new SpringLayout();
+		this.app = appController;
+		
+		this.appLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
