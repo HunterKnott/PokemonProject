606c6484a7ff14d2b6c01872f607eaa01b08a045
HunterKnott <hunterknott00@gmail.com>
2019-02-14 11:44:21 -0700

    Made constructors for the Charmander class

--- src/poke/model/Charmander.java
+++ src/poke/model/Charmander.java
@@ -1,13 +1,23 @@
 package poke.model;
 
-public class Charmander extends Pokemon implements Fire
+public class Charmander extends Pokemon //implements Fire
 {
+	public Charmander()
+	{
+		super(4, "Charmander");
+	}
+	
+	public Charmander(String name)
+	{
+		super(4, name);
+	}
+	
 	public Charmander(int number, String name)
 	{
 		super(number, name);
 	}
 	
-	public int flamethrower()
+	/*public int flamethrower()
 	{
 		
 	}
@@ -15,5 +25,5 @@ public class Charmander extends Pokemon implements Fire
 	public boolean will_o_wisp()
 	{
 		
-	}
+	}*/
 }
